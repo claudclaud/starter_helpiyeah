ac395ae8b45cb692c8bea8e41c29c83adbb4007b
Claudia Chance <cchance@udel.edu>
2024-10-08 10:22:28 -0400

    adding this to main

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          claudia chance is here yayay
         </p>
         <a
           className="App-link"
